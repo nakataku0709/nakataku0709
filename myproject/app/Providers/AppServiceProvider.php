ea7c2910d8b93775b8ce6301794cf3b73d38f458
nakataku0709 <soccer.tuesday.0709@ezweb.ne.jp>
2023-11-01 22:05:28 +0900

    ブログ投稿一覧のページネーション追加

--- myproject/app/Providers/AppServiceProvider.php
+++ myproject/app/Providers/AppServiceProvider.php
@@ -3,10 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
     /**
      * Register any application services.
      *
@@ -15,15 +24,5 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Paginator::useBootstrap();
     }
 }
